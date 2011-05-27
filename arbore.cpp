d8e518fd1e4b11fd428513ed159a71a763bdcc5c
Titus Nicolae <nicolaetitus12@gmail.com>
2011-05-27 19:25:39 +0300

    certainly advancing somewhere

--- arbore.cpp
+++ arbore.cpp
@@ -4,15 +4,23 @@
 
 using namespace std;
 class node
-{ bool isoperator;  //if it is a variable, or it's an operator 
+{
+private:
+  bool isOperator;  //if it is a variable, or it's an operator 
   char *s;    //variable name or the name of the operator
   node *parent;    //parent
+
+public:
   vector<node*> list; //list of children
-  public:
-  node(bool isoperator, char *s)
-  { this->isoperator=isoperator;
+  bool minus;
+  bool inverse;
+
+  node(bool isOperator, char *s,bool minus=false,bool inverse=false)
+  { this->isOperator=isOperator;
     this->s=s;
     this->parent=NULL;
+    this->minus=minus;
+    this->inverse=inverse;
   }
 
   void addChild(node *p)
@@ -21,7 +29,8 @@ class node
   }
 
   void print()
-  { if(this->isoperator)
+  {
+    if(this->isOperator)
     { if(this->list.size()==1)
       { char *paren[2]={strdup(""),strdup("")};
 
@@ -32,7 +41,7 @@ class node
         printf("%s%s",this->s,paren[0]);
         this->list[0]->print();
         printf("%s",paren[1]);
-      }
+      
       else if(this->list.size()>1)
       { char *paren[2]={strdup(""),strdup("")};
 
@@ -55,10 +64,11 @@ class node
     { printf("%s",this->s);
     }
   }
+
   static int priority(node *n)
   { if (n==NULL)
       return 0;
-    if(n->isoperator) 
+    if(n->isOperator) 
     { if(!strcmp(n->s,"+") || !strcmp(n->s,"-"))
         return 1; 
       if(!strcmp(n->s,"*") || !strcmp(n->s,"/"))
@@ -71,62 +81,161 @@ class node
     else 
       return 5;
   }
+
   char* gets()
   { return s;
+  }
+
+  bool getParent()
+  { return this->parent;
+  }
+
+  bool hasParent()
+  { if(this->p==NULL)
+      return false;
+    return true;
+  }
+  void updateSign(node *t)
+  { this->minus^=t->getSign();
+    t->setSign(false);
+  }
+  void setSign(bool flag)
+  { this->minus=flag;
+  }
+  bool getSign()
+  { return this->minus;
+  }
+  bool isOperand()
+  { return !this->isOperator;
   }
 };
 
 
-void process(char* &c)
-{ node *p=NULL;
-  char s1,s2;
+node* process(char* &c,bool coa)
+{ 
+  node *p=NULL;//last operator or first operand
+  char s1,s2,*func;
+  bool wasminus=false;//todo dezactivare
+  bool wasinverse=false;//la fel
+  bool wasoperand=false;//daca ultima chestia a fost un operand
+
   while(c!=NULL)
   { 
     if(*c=='(' || *c=='[')
-    { 
+    { wasoperand=true;
+      c++;
+      node* r=process(c);
     }
     else if(*c==')' || *c==']')
     {
+      c++;
+      node *u=p;
+      while(u->hasParent())
+      { u=u->getParent();   
+      } 
     }
-    else if(*c=='+')
+    else if(*c=='+'||*c=='-')
     { 
+      { 
+        node *o=new node(true,strdup("+"));
+        if(node::priority(p)==node::priority(o))//daca este sir de +-+-
+        { c++; 
+        }
+        else //daca + trebuie pus deasupra de * sau ^
+        { node *u=p;
+          while(node::priority(o)<node::priority(u))
+          { u=u->getParent();
+          }
+          if(node::priority(u)==node::priority(o))//era un + inainte de sirul ala de *
+          { p=u;
+            c++;
+          }
+          else//trebuie pus un + deasupra de * 
+          { o.addChild(u); 
+            o.updateSign(u); 
+            p=o;
+          }
+        }
+      }
+      wasoperand=false;//trebuie la sfarsit neaparat!!
     }
-    else if(*c=='-')
-    {
-    }
-    else if(*c=='/')
-    {
-    }
-    else if(*c==' ') 
-    {
+    else if(*c==' '||*c=='/')
+    { 
+      wasoperand=false; 
+      //creaza nod '*'
+      if(p==NULL) printf("hopa penelopa");
+      node *o=new node(true,strdup("*"));
+      if(p->isOperand())
+      { o.addChild(p);
+        o.updateSign(p);
+        p=o; 
+      }
+      else
+      { if(node::priority(p)==node::priority(o))//e un sir de * 
+        { c++; 
+        }
+        else if(node::priority(p)>node::priority(o)) //* trebuie pus deasupra de ^ si poate dedesubt de + 
+        {   
+          node *u=p;
+          while(node::priority(u)>node::priority(o)&&u->hasParent())//urca de jos in sus pana ajunge la * sau sub *
+          { u=u->getParent();                    
+          }
+          if (node::priority(u)==node::priority(o))//daca a ajuns la * dupa ce a urcat
+          { p=u;
+          }
+          else //daca a ajuns sub * dupa ce a urcat
+          { 
+            if(u->hasParent())//daca exista + atunci
+            { node* h=u->getParent();
+              h->list.pop_back();
+              h.addChild(o);
+            }
+            o.addChild(u); //* adauga pe ^ ca fiu
+            o.updateSign(u); 
+            p=o;
+          }  
+          c++; 
+        }
+        else //trebuie pus * sub + si furat de la + un termen
+        { node *t=p->list.back();
+          p->list.pop_back();
+          p.addChild(o); 
+          o.addChild(t);
+          o.updateSign(t);
+          p=o;
+          c++;
+        }
+      }
     }
     else if(*c=='^')
-    {
-    }
-    else if(*c>='A' && *c<='Z')
-    {
+    { wasoperand=false; 
     }
     else if((*c>=(s1='a') && *c<=(s2='z'))||(*c>=(s1='0') && *c<=(s2='9')))
-    { for (char *t=c; *t>=s1 && *t<=s2;t++);
-      char *s=new char[t-c+1];
+    { 
+      wasoperand=true; 
+      char *t;
+      for (t=c; *t>=s1 && *t<=s2;t++);
+      char *s=new char[t-c+1]; //+1 pentru NULL
       strncpy(s,c,t-c);
-      s[t-c]=0;
-      node *o=new node(false,s);
-      ///
-      if(p!=NULL)
-        p.addChild(o);  
+      s[t-c]=0; //pune NULL la sfarsitul string-ului
+      c=t;
+      node *o=new node(false,s,wasminus,wasinverse);//nu e operator, baga sirul
+      wasminus=wasinverse=false;
+      if (p==NULL)
+      { p=o;
+      }    
       else
-        p=o;
+      { p.addChild(o);
+      }
+    }
+    else if(!strncmp((func="Sin"),p,4) || !strncmp((func="Cos"),p,3) || !strncmp((func="Abs"),p,3))
+    { wasoperand=true; 
     }
     else
-    { 
+    { printf("%c wtf is this shit",*c);
     }
   }
-  if(p!=NULL)
-  { for (
-     
-  } 
-  return r;
+  return p;
 }
 
 void test()
@@ -148,7 +257,7 @@ void test()
 
 int main ()
 { char *line,*c;
-  FILE *fin=fopen("math.in","r");
+  FILE *fin=fopen("math2.in","r");
   line=new char[140000];  
   fgets(line,140000,fin);
   node *n[6];
@@ -157,7 +266,9 @@ int main ()
     while(c!=NULL)
     { printf("%s",c);
       n[i]=process(c);
+      c=strtok(NULL,"\n");
     } 
   }
   return 0;
 }
+
--- test.cpp
+++ test.cpp
@@ -1,10 +1,18 @@
 #include <stdio.h>
+#include <vector>
+using namespace std;
 int main ()
-{ char s[]="ana are 123 mere si ce daca";
-  char *c;
-  for (c=s; !(*c>='0' && *c <='9');c++);
-  char *p;
-  for (p=c; *p>='0' && *p<='9';p++);
-  printf("%d",p-c); 
+{ vector<int> a;
+  for(int i=0;i<10;i++)
+  { a.push_back(i);
+  }
+  for (int i=0;i<a.size();i++) 
+    printf("%d",a[i]);
+  printf("%d",a.back());
+  a.pop_back();
+  printf("\n");
+  for (int i=0;i<a.size();i++)
+  { printf("%d",a[i]);
+  }
   return 0;
 }
